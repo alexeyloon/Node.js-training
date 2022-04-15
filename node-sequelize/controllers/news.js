b671aa0d06933f0145e0782b6fc9e2aa71875633
alexeyloon <alexeyloon921@gmail.com>
2022-04-15 15:44:53 +0300

    add pagination on news list

--- node-sequelize/controllers/news.js
+++ node-sequelize/controllers/news.js
@@ -1,6 +1,39 @@
 const News = require('../models').News;
 
+const getPagination = (page, size) => {
+    const limit = size ? +size : 3;
+    const offset = page ? page * limit : 0;
+
+    return { limit, offset };
+};
+
+const getPagingData = (data, page, limit) => {
+    const { count: totalItems, rows: tutorials } = data;
+    const currentPage = page ? +page : 0;
+    const totalPages = Math.ceil(totalItems / limit);
+
+    return { totalItems, tutorials, totalPages, currentPage };
+};
+
 module.exports = {
+    findAll(req, res) {
+        const { page, size, title } = req.query;
+        var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+
+        const { limit, offset } = getPagination(page, size);
+
+        News.findAndCountAll({ where: condition, limit, offset })
+            .then(data => {
+                const response = getPagingData(data, page, limit);
+                res.send(response);
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message:
+                        err.message || "Some error occurred while retrieving tutorials."
+                });
+            });
+    },
     list(req, res) {
         return News
             .findAll({
